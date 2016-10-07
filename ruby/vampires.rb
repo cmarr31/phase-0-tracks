02bb2a8fb99fc1e6849c645e425fac868385ceb0
cmarr31 <chris@sitefuelusa.com>
2016-10-07 14:13:22 -0500

    Add loop for number of employees.

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,53 +1,65 @@
-puts "What is your name?"
-name = gets.chomp
-if name == "Drake Cula"
-	name = false
-elsif name == "Tu Fang"
-	name = false
-else
-	name = true
-end
+puts "How many employees?"
+employees = gets.chomp
+times = 0 
 
-puts "How old are you?"
-age = gets.chomp
-Integer(age)
-
-puts "What year were you born?"
-year = gets.chomp
-Integer(year)
-if 2016 - Integer(year) == Integer(age)
-	age_year = true
-else
-	age_year = false
-end
-
-puts "We order garlic bread here for our employees regularly, should we put you on the list? (y/n)"
-garlic_bread = gets.chomp
-if garlic_bread == "y"
-	garlic_bread = true
-else
-	garlic_bread = false
-end
-
-puts "Would you like to enroll in our company's health insurance? (y/n)"
-insurance = gets.chomp
-if insurance == "y"
-	insurance = true
-else
-	insurance = false
-end
-
-message = case
-when !name
-	 result = "Definitely a vampire."
-when age_year && (garlic_bread || insurance)
-	 result = "Probably not a vampire."
-when !age_year && !garlic_bread && !insurance
-	 result = "Almost certainly a vampire."
-when !age_year && (!garlic_bread || !insurance)
-	result = "Probably a vampire."
-else
+begin
+	puts "What is your name?"
+	name = gets.chomp
+	if name == "Drake Cula"
+		name = false
+	elsif name == "Tu Fang"
+		name = false
+	else
+		name = true
+	end
+	
+	puts "How old are you?"
+	age = gets.chomp
+	Integer(age)
+	
+	puts "What year were you born?"
+	year = gets.chomp
+	Integer(year)
+	if 2016 - Integer(year) == Integer(age)
+		age_year = true
+	else
+		age_year = false
+	end
+	
+	puts "We order garlic bread here for our employees regularly. Should we put you on the list? (y/n)"
+	garlic_bread = gets.chomp
+	if garlic_bread == "y"
+		garlic_bread = true
+	else
+		garlic_bread = false
+	end
+	
+	puts "Would you like to enroll in our company's health insurance? (y/n)"
+	insurance = gets.chomp
+	if insurance == "y"
+		insurance = true
+	else
+		insurance = false
+	end
+	
 	result = "Results inconclusive."
-end
-
-puts result
+	
+	if age_year && (garlic_bread || insurance)
+		 result = "Probably not a vampire."
+	end
+	
+	if	!age_year && (!garlic_bread || !insurance)
+		result = "Probably a vampire."
+	end
+	
+	if	!age_year && !garlic_bread && !insurance
+		result = "Almost certainly a vampire."
+	end
+	
+	if	!name
+		result = "Definitely a vampire."
+	end
+	
+	puts result	
+	times +=1
+end while times < Integer(employees)
