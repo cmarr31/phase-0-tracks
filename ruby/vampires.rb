2836b0c751af7e9c378eac2bd020f0aaa54d7887
cmarr31 <chris@sitefuelusa.com>
2016-10-07 11:24:37 -0500

    Add preliminary new hire questions and receive answers.

---
+++ ruby/vampires.rb
@@ -0,0 +1,16 @@
+puts "What is your name?"
+name = gets.chomp
+
+puts "How old are you?"
+age = gets.chomp
+Integer(age)
+
+puts "What year were you born?"
+year = gets.chomp
+Integer(year)
+
+puts "We order garlic bread here for our employees regularly, should we put you on the list?"
+garlic_bread = gets.chomp
+
+puts "Would you like to enroll in our company's health insurance?"
+insurance = gets.chomp
