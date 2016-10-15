3e69e81014738a95290cc4a9b52fef2c4b636977
cmarr31 <chris@sitefuelusa.com>
2016-10-15 16:22:13 -0500

    Construct array.

---
+++ ruby/hash_practice.rb
@@ -0,0 +1,12 @@
+app_data = {
+	name: "Joe M.",
+	address: "111 Anyroad Ln",
+	email: "joem@fmail.com",
+	phone: "123-456-7890",
+	fav_shade: "cerulean",
+	paper_pref: "Paisley",
+	Ombre: "So last season",
+}
+
+app_data[:hair_color] = "brown"
+app_data[:hired] = "yes"
