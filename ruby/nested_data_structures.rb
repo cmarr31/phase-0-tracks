33f2d5eede97a3c8041be1e40bc5888904f4a873
cmarr31 <chris@sitefuelusa.com>
2016-10-16 12:12:04 -0500

    Construct a nested data structure regarding living organisms.

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,40 @@
+
+
+
+
+
+living_organisms = {
+	plants: {
+		forest: [
+			'shrubs',
+			'wildflowers',
+			'ferns',
+			'mosses'
+		]
+		marine: [
+			'algae',
+			'kelp',
+			'seaweed'
+		]
+		desert: [
+			'tumbleweed',
+			'cacti',
+		]
+	}
+	animals: {
+		forest: [
+			'birds',
+			'foxes',
+			'deer'
+		]
+		marine: [
+			'sharks',
+			'coral'
+		]
+		desert: [
+			'camels',
+			'snakes',
+			'scorpions'
+		]
+	}
+}
