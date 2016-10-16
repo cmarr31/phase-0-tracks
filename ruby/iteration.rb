853de0dcbe9eb4b9703bc1ee8090d4095860207a
cmarr31 <chris@sitefuelusa.com>
2016-10-15 19:17:31 -0500

    Iterate through an array and a hash with .each and .map

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -5,3 +5,27 @@ def whats_up
 end
 
 whats_up { |name1| puts "Not much #{name1}." }
+
+
+colors = ["red", "blue", "green", "yellow"]
+
+names = {'chris' => 'marr', 'joe' => 'peterson', 'john' => 'pitlock', 'ryan' => 'callaway'}
+
+p colors
+colors.each do |item|
+	puts item
+end
+p colors
+
+p names
+names.each do |first, last|
+	puts "First name: #{first}. Last name: #{last}."
+end
+p names
+
+p colors
+colors.map! do |color|
+	puts color
+	color.upcase
+end
+p colors
