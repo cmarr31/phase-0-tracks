e63455496025be8a6330a468bfc24734846cc320
cmarr31 <chris@sitefuelusa.com>
2016-10-16 19:48:47 -0500

    Implement insertion sort.

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -24,3 +24,17 @@ end
 
 fib(100)
 
+# ------------------------------------
+
+def insertion(arr)
+    for size in 2..arr.length
+        element = arr[size-1]   
+        index = size - 2
+        while(index >= 0) && (element < arr[index])
+            arr[index + 1] = arr[index]
+            index-=1
+        end
+        arr[index+1] = element
+    end
+        
+end
