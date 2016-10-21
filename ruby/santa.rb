f629a4d69c3f326772da9b0db80ed59fc5a6ada9
cmarr31 <chris@sitefuelusa.com>
2016-10-21 17:31:08 -0500

    Create Santa class and construct first three instance methods.

---
+++ ruby/santa.rb
@@ -0,0 +1,20 @@
+class Santa
+end
+
+class Santa
+	def speak
+		puts "Ho! Ho! Ho! Haaaappy holidays!"
+	end
+end
+
+class Santa
+	def eats_milk_and_cookies(str)
+		puts "That was a good #{str}!"
+	end
+end
+
+class Santa
+	def initialize
+		puts "Initializing santa instance..."
+	end
+end
