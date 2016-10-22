77465dbfa0625f9881670876f28d5ba8ca5c7e95
cmarr31 <chris@sitefuelusa.com>
2016-10-21 21:28:45 -0500

    Construct Shout mix in method and two classes.

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,11 +1,28 @@
+# module Shout
+# 	 def self.yell_angrily(words)
+# 	 	words + "!!!" + " :("
+# 	 end
+# 	 def self.yelling_happily(words)
+# 	 	words + "!!!" + ":]"
+# 	 end
+# end
+# 
+# p Shout.yell_angrily("Dude")
+# p Shout.yelling_happily("Dude")
+
 module Shout
-	def self.yell_angrily(words)
-		words + "!!!" + " :("
+	def yell_angrily(words)
+		words + "!!!" + ":("
 	end
-	def self.yelling_happily(words)
+	def yelling_happily(words)
 		words + "!!!" + ":]"
 	end
 end
 
-p Shout.yell_angrily("Dude")
-p Shout.yelling_happily("Dude")
+
+class Monkey
+	include Shout
+end
+class Athlete
+	include Shout
+end
