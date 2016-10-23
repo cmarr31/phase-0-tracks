5a66eafe744111d8e19e34ca84b11b274a2721d0
cmarr31 <chris@sitefuelusa.com>
2016-10-23 11:41:16 -0500

    Moved much code from hangman.rb into game.rb

--- ruby/solo_6.6/game.rb
+++ ruby/solo_6.6/game.rb
@@ -3,16 +3,26 @@ class Game
 
 	def initialize
 		@guess_count = 0
-		@is_over = false
+		@is_over = FALSE
 	end
 
-	def try(guess1)
+	def try(guess1, word1)
+		count = word1.length
 		@guess_count +=1
 		if word1.include? guess1
 			word1.delete!(guess1)
 			puts word1.sub(/[abcdefghijklmnopqrstuvwxyz]/, '-')
 		else
 			puts "Nope! Try again."
+		end
+		if word1.length == 0 
+			@is_over = TRUE
+			puts "Congratulations you're a winner!!! Keep on winning ;)"
+		elsif @guess_count == count
+			@is_over = TRUE
+			puts "Just add this to the (I'm assuming) your already long list of miserable failures :'("
+		else
+			FALSE
 		end
 	end
 end
--- ruby/solo_6.6/hangman.rb
+++ ruby/solo_6.6/hangman.rb
@@ -5,14 +5,8 @@ puts "Welcome to hangman!"
 puts "Player One, enter a word for Player Two to guess:"
 word1 = gets.chomp
 
-until word1.length == 0 || @guess_count == word1.length do 
+while !game.is_over
 	puts "Player Two, guess a letter."
 	guess1 = gets.chomp
-	game.try(guess1)
-end
-
-if word1.length == 0
-	puts "Congratulations you're a winner!!! Keep on winning ;)"
-else
-	puts "Just add this to the (I'm assuming) your already long list of miserable failures :'("
+	game.try(guess1, word1)
 end
