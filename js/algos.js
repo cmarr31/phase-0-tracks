d63e722b6f0fe56bc07b20ba0d9dfcd705e7e733
cmarr31 <chris@sitefuelusa.com>
2016-10-30 15:02:37 -0500

    Pseudocode a prospective solution to Release 0.

---
+++ js/algos.js
@@ -0,0 +1,13 @@
+// Write a function that takes an array of words or phrases and 
+// returns the longest word or phrase in the array.
+// 1) Find the length of each word in the array.
+// 2) Put those integers in a separate array.
+// 3) Print the word in the original array that is at the same index
+//     as the highest number in the second array.
+
+
+function Words(arr) {
+	this.longest = function() {
+
+	}
+}
