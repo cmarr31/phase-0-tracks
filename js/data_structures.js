c1f65be367cec21c97017c1d2c0c008383ff7d8d
cmarr31 <chris@sitefuelusa.com>
2016-10-29 22:05:24 -0500

    Create loop to add name and associated color to object 'horses'.

--- js/data_structures.js
+++ js/data_structures.js
@@ -3,3 +3,11 @@ var names = ["Joe", "Ryan", "Ted", "John"];
 
 colors.push("orange");
 names.push("Philip");
+
+var horses = {};
+
+for (var i = 0; i < names.length; i++) {
+	horses[names[i]] = colors[i];
+}
+
+console.log(horses);
