8f31778ec954315ed5d864a2422b7d75a6a6d413
cmarr31 <chris@sitefuelusa.com>
2016-11-07 01:13:39 -0600

    Add strings to be printed to user's terminal to prompt them to enter information. Construct if/else & until loop to find out what the user wants to do (add, edit, or delete), and add methods to accomplish what the user wants.

--- combining_sql_ruby/motivator.rb
+++ combining_sql_ruby/motivator.rb
@@ -14,16 +14,12 @@ db = SQLite3::Database.new("motivation.db")
 create_table_cmd = <<-SQL 
 	CREATE TABLE IF NOT EXISTS motivators(
 		id INTEGER PRIMARY KEY,
-		name VARCHAR(255),
-		age INT
+		quote VARCHAR(255),
+		name VARCHAR(255)
 	)
 SQL
 
 db.execute(create_table_cmd)
-
-def add_quote(db, quote, name)
-	db.execute("INSERT INTO motivators (quote, name) VALUES (?, ?)", [name,age])
-end
 
 puts "Welcome to your personal Automated Self-Motivator(TM)!!!"
 puts "RANDOM MOTIVATING QUOTE FROM DATABASE"
@@ -31,24 +27,37 @@ puts "RANDOM MOTIVATING QUOTE FROM DATABASE"
 puts "What would you like to do with your quotes? Type: 'add', 'edit', or 'delete'"
 gets.chomp = initial_response
 
+until initial_response == ("add") || ("edit") || ("delete") do
 
 	if initial_response == "add"
-		def method_name
-			
+		puts "Please type the quote:"
+		quote = gets.chomp
+		puts "Please type the name of the person who said this:"
+		name = gets.chomp
+		def add_quote(db, quote, name)
+			db.execute("INSERT INTO motivators (quote, name) VALUES (?, ?)", [name,age])
 		end
 	elsif initial_response == "edit"
-		def method_name
-			
+		puts "Please type the ID # of the quote you'd like to revise:"
+		id1 = gets.chomp
+		puts "Please type the revised quote:"
+		revised_quote = gets.chomp
+		def edit_quote(db, revised_quote, id1)
+			db.execute("UPDATE motivators SET quote=revised_quote WHERE id=id1")
 		end
 	elsif initial_response == "delete"
-		def method_name
-			
+		puts "Please type the ID # of the quote you'd like to delete:"
+		id2 = gets.chomp
+		def delete_quote(db, id2)
+			db.execute("DELETE FROM motivators WHERE id=id2")
 		end
 	else
 		puts "I'm sorry, I didn't understand that."
 	end
+end
 
-
+puts "Thank you for using your personal Automated Self-Motivator(TM)!!!"
+puts "RANDOM MOTIVATING QUOTE FROM DATABASE"
 
 
 
