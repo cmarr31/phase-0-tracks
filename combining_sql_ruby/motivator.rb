b2a7b744eff86810da3a53ce588ed9c61cf871d8
cmarr31 <chris@sitefuelusa.com>
2016-11-06 23:55:00 -0600

    Write code to create database, & to create table 'motivators'. Construct method for adding a quote (with associated name) to the table.

--- combining_sql_ruby/motivator.rb
+++ combining_sql_ruby/motivator.rb
@@ -7,3 +7,69 @@
 # Every time they turn on/turn off their Motivator, they should be
 # prompted with a random quote from their database.
 
+require 'sqlite3'
+
+db = SQLite3::Database.new("motivation.db")
+
+create_table_cmd = <<-SQL 
+	CREATE TABLE IF NOT EXISTS motivators(
+		id INTEGER PRIMARY KEY,
+		name VARCHAR(255),
+		age INT
+	)
+SQL
+
+db.execute(create_table_cmd)
+
+def add_quote(db, quote, name)
+	db.execute("INSERT INTO motivators (quote, name) VALUES (?, ?)", [name,age])
+end
+
+puts "Welcome to your personal Automated Self-Motivator(TM)!!!"
+puts "RANDOM MOTIVATING QUOTE FROM DATABASE"
+
+puts "What would you like to do with your quotes? Type: 'add', 'edit', or 'delete'"
+gets.chomp = initial_response
+
+
+	if initial_response == "add"
+		def method_name
+			
+		end
+	elsif initial_response == "edit"
+		def method_name
+			
+		end
+	elsif initial_response == "delete"
+		def method_name
+			
+		end
+	else
+		puts "I'm sorry, I didn't understand that."
+	end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
