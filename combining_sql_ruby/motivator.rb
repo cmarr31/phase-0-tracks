d91384a1d18c0434a64467d6422808bd386a90de
cmarr31 <chris@sitefuelusa.com>
2016-11-06 23:23:55 -0600

    Created directory for my response to challenge 8.5.

---
+++ combining_sql_ruby/motivator.rb
@@ -0,0 +1,9 @@
+# The Automated Self-Motivator #
+
+# Create a program that allows a user to store motivating quotes, sayings
+# and phrases along with the person who said the thing.
+# They will need to be able to access, change, update, and edit all of 
+# them at any time on command.
+# Every time they turn on/turn off their Motivator, they should be
+# prompted with a random quote from their database.
+
